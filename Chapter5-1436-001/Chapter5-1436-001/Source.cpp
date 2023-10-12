3cd6826b2515a183c18586132a7ad2393fb5ec89
Work <Work@MSI>
2023-10-12 09:53:38 -0500

    Where we ended on Oct 12 in the morning

--- Chapter5-1436-001/Chapter5-1436-001/Source.cpp
+++ Chapter5-1436-001/Chapter5-1436-001/Source.cpp
@@ -4,7 +4,30 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World";
+    //LOOPS/repetition 
+
+    const int TOTAL_NUMBER_OF_STUDENTS = 5; 
+
+    int currentStudentNumber = 1; //"loop counter variable" 
+    
+    int currentStudentGrade;  //declare 
+
+    double sumOfAllGrades = 0; 
+
+    while (currentStudentNumber <= TOTAL_NUMBER_OF_STUDENTS)
+    {
+        cout << "Enter student number " << currentStudentNumber << " 's grade: " << endl; 
+        cin >> currentStudentGrade; 
+
+        sumOfAllGrades = sumOfAllGrades + currentStudentGrade; 
+
+        currentStudentNumber++; //C++!
+
+    }
+
+    double average = sumOfAllGrades / 5; 
+
+    cout << "\n\nAverage: " << average << endl; 
 
     return 0;
 }
